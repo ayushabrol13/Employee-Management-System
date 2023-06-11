609497f6d2a08a2f8c53312044a9ed8052172763
nc24634_amit <kumaramit@ninjacart.com>
2023-06-11 21:35:14 +0530

    Created a Caffeine cache CustomeKeyGenerator is not used currently( can be deprecated )

--- src/main/java/com/bfb/emprepository/controller/EmployeesController.java
+++ src/main/java/com/bfb/emprepository/controller/EmployeesController.java
@@ -1,17 +1,27 @@
 package com.bfb.emprepository.controller;
 
+import com.bfb.emprepository.customKey.CustomKeyGenerator;
 import com.bfb.emprepository.models.Employees;
 import com.bfb.emprepository.services.EmpService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+//cache config
+@CacheConfig(cacheNames = {"employees"})
 public class EmployeesController {
 
     @Autowired
     private EmpService empService;
+
+//    @Autowired
+//    private CustomKeyGenerator customKeyGenerator;
 
     @PostMapping("/employees")
     public Employees createEmployee(@RequestBody Employees employee) {
@@ -24,16 +34,19 @@ public class EmployeesController {
     }
 
     @GetMapping("/employees/{eid}")
+    @Cacheable(key = "#eid")
     public Employees fetchEmployee(@PathVariable("eid") int eId) {
         return empService.fetchDepartmentById(eId);
     }
 
     @PutMapping("/employees")
+    @CachePut
     public Employees updateEmployee(Employees emp) {
         return empService.updateEmployee(emp);
     }
 
     @DeleteMapping("/employees/{eid}")
+    @CacheEvict(key = "#eid", allEntries = true)
     public String deleteEmployee(@PathVariable("eid") int empId){
         empService.deleteEmployee(empId);
         return "Employee with id " + empId + " got deleted successfully...";
