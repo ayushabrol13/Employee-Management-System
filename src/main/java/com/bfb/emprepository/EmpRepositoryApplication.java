cdd866cfe30e615d642068017743f313a8d2c898
nc24634_amit <kumaramit@ninjacart.com>
2023-06-12 19:32:39 +0530

    Created caffeine cache on service layer

--- src/main/java/com/bfb/emprepository/EmpRepositoryApplication.java
+++ src/main/java/com/bfb/emprepository/EmpRepositoryApplication.java
@@ -2,10 +2,12 @@ package com.bfb.emprepository;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.bfb.emprepository.dao")
+@EnableCaching
 public class EmpRepositoryApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/bfb/emprepository/services/impl/EmpServiceImpl.java
+++ src/main/java/com/bfb/emprepository/services/impl/EmpServiceImpl.java
@@ -6,11 +6,21 @@ import com.bfb.emprepository.exceptions.InputFieldsEmptyException;
 import com.bfb.emprepository.exceptions.ResourceNotFoundException;
 import com.bfb.emprepository.models.Employees;
 import com.bfb.emprepository.services.EmpService;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 import java.util.*;
 
 @Service
+@CacheConfig(cacheNames = {"employees"})
 public class EmpServiceImpl implements EmpService {
 
     @Autowired
@@ -22,11 +32,13 @@ public class EmpServiceImpl implements EmpService {
     }
 
     @Override
+    @Cacheable(cacheNames = "employees")
     public Employees fetchEmployeeById(int eId) {
         return employeeRepo.findById(eId).orElseThrow(() -> new ResourceNotFoundException("Employee", "Id", eId));
     }
 
     @Override
+    @CachePut(cacheNames = "employees")
     public Employees updateEmployee(Employees employee) {
         Employees existingEmp = employeeRepo.findById(employee.getEmpId()).orElseThrow(() -> new ResourceNotFoundException("Employee", "Id", employee.getEmpId()));
         if(employee.getDepartment().equalsIgnoreCase("") || employee.getName().equalsIgnoreCase("")|| employee.getEmpId()==0)
@@ -39,6 +51,7 @@ public class EmpServiceImpl implements EmpService {
     }
 
     @Override
+    @CachePut(cacheNames = "employees")
     public Employees createEmployee(Employees employee) {
         if(employee.getDepartment().equalsIgnoreCase("") || employee.getName().equalsIgnoreCase("")|| employee.getEmpId()==0)
             throw new InputFieldsEmptyException();
@@ -46,6 +59,7 @@ public class EmpServiceImpl implements EmpService {
     }
 
     @Override
+    @CacheEvict(cacheNames = "employees", allEntries = true)
     public String deleteEmployee(int empId) {
         Employees existingEmp = employeeRepo.findById(empId).orElseThrow(() -> new ResourceNotFoundException("Employee", "Id", empId));
         employeeRepo.delete(existingEmp);
