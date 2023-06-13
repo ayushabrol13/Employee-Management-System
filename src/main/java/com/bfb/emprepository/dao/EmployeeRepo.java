45c07a718d8d801b2e7edb8e83c92b760d4b3aba
harikesh <harikesh@ninjacart.com>
2023-06-13 11:43:34 +0530

    Repo layer done...

--- src/main/java/com/bfb/emprepository/dao/EmployeeRepo.java
+++ src/main/java/com/bfb/emprepository/dao/EmployeeRepo.java
@@ -2,10 +2,13 @@ package com.bfb.emprepository.dao;
 
 import com.bfb.emprepository.models.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+//Annotation for Repo layer
+@Repository
 public interface EmployeeRepo extends JpaRepository<Employees, Integer> {
 
-    public List<Employees> findByDepartmentIgnoreCase(String depName);
+    public List<Employees> findByDepartmentIgnoreCase(String dep);
 }
