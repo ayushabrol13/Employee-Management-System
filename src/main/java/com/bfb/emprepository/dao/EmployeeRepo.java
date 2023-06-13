9a1b41f9806fcb5fda2100a711596e780c4bd83f
harikesh <harikesh@ninjacart.com>
2023-06-13 11:40:52 +0530

    Repository Layer improvised...

--- src/main/java/com/bfb/emprepository/dao/EmployeeRepo.java
+++ src/main/java/com/bfb/emprepository/dao/EmployeeRepo.java
@@ -2,11 +2,10 @@ package com.bfb.emprepository.dao;
 
 import com.bfb.emprepository.models.Employees;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface EmployeeRepo extends JpaRepository<Employees, Integer> {
+
     public List<Employees> findByDepartmentIgnoreCase(String depName);
 }
