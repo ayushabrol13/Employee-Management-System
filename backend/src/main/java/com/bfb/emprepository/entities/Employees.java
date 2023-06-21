b54a201e89469edba6d24cdb826ba1ee365b040e
NC24632_Anubhav Modi <nc24632_anubhavmodi@Ninja-ITs-MacBook-Air-3.local>
2023-06-21 17:18:49 +0530

    IDs made universal and Sequence is Generated...

--- backend/src/main/java/com/bfb/emprepository/entities/Employees.java
+++ backend/src/main/java/com/bfb/emprepository/entities/Employees.java
@@ -16,8 +16,16 @@ import lombok.NoArgsConstructor;
 public class Employees {
 
     @Id
-    @SequenceGenerator(name = "empIdGenerator")
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(
+            name = "emp_id_seq",
+            sequenceName = "emp_id_seq",
+            allocationSize = 1,
+            initialValue = 24600
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "emp_id_seq"
+    )
     private Integer empId;
 
     @Column(name = "name", nullable = false, length = 50)
